ca6b77b4f57e7ab54b4f45cdef5f1f06d47219eb
Markel <m.ryabtsev@outlook.com>
2021-11-03 10:13:47 +0600

    Added 7 task

--- task7.py
+++ task7.py
@@ -16,9 +16,23 @@ class Task7:
     def start_task(self):
         helper = Helper()
         print(f'------------------------- Задача {self.task_number} -------------------------')
-        print('Вычислить значение функции ')
+        print('Вычислить значение функции y = ae^(−ax) * sin(ωx) при x = (π / 2 − φ) / ω')
 
+        a = helper.set_value_simple('a')
+        omega = helper.set_value_simple('ω', 0, True)
+        phi = helper.set_value_simple('φ')
 
+        x = self.__get_x(a, omega, phi)
+        y = self.__get_y(a, omega, x)
 
+        print(f'y = {y}, x = {x}')
         print('----------------------------------------------------------')
         self.task_ended_callback(self.task_number)
+
+    @staticmethod
+    def __get_x(a: float, omega: float, phi: float) -> float:
+        return round((((math.pi / 2) * phi) / omega), 2)
+
+    @staticmethod
+    def __get_y(a: float, omega: float, x: float) -> float:
+        return round((a * math.exp(-a * x) * math.sin(omega * x)), 4)
